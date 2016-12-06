8ea5c2f571f4810d0f4baad1e1f04d563b7831c1
谢欣言 <495094655@qq.com>
2016-12-06 22:39:15 +0800

    Button

---
+++ Hana/app/src/main/java/com/ciranshu/hana/Main2Activity.java
@@ -0,0 +1,34 @@
+package com.ciranshu.hana;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.view.View;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class Main2Activity extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+        Button btn2_1=(Button)findViewById(R.id.Btn2_1);
+        Button btn2_2=(Button)findViewById(R.id.Btn2_2);
+        Button btn2_3=(Button)findViewById(R.id.Btn2_3);
+        Button btn2_4=(Button)findViewById(R.id.Btn2_4);
+        Button btn2_5=(Button)findViewById(R.id.Btn2_5);
+        Button btn2_6=(Button)findViewById(R.id.Btn2_6);
+        Button btn2_7=(Button)findViewById(R.id.Btn2_7);
+        Button btn2_8=(Button)findViewById(R.id.Btn2_8);
+        Button btn2_9=(Button)findViewById(R.id.Btn2_9);
+    }
+}
+
