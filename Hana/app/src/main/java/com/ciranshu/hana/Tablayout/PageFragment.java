7a581bbd13bd0b7b6771cf95802cfc585314d018
HukiOne <wsyj776@163.com>
2016-12-08 23:14:23 +0800

    Outside布局

--- Hana/app/src/main/java/com/ciranshu/hana/Tablayout/PageFragment.java
+++ Hana/app/src/main/java/com/ciranshu/hana/Tablayout/PageFragment.java
@@ -1,14 +1,19 @@
 package com.ciranshu.hana.Tablayout;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.ciranshu.hana.MainActivity;
 import com.ciranshu.hana.R;
 
 
@@ -16,17 +21,20 @@ public class PageFragment extends Fragment {
 
     public static final String ARG_PAGE = "ARG_PAGE";
     private int mPage;
-
+    private ImageButton flower1;
     public static PageFragment newInstance(int page) {
+
         Bundle args = new Bundle();
         args.putInt(ARG_PAGE, page);
         PageFragment pageFragment = new PageFragment();
         pageFragment.setArguments(args);
+
         return pageFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         mPage = getArguments().getInt(ARG_PAGE);
     }
@@ -34,23 +42,57 @@ public class PageFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.inside, container, false);
+        View view = inflater.inflate(R.layout.outside, container, false);
         if(mPage==1)
         {
            view = inflater.inflate(R.layout.inside, container, false);
-
+            //flower1 = (ImageButton)findViewById(R.id.imageButton1);
         }
-        else if(mPage==2)
-        {
-           view = inflater.inflate(R.layout.outside, container, false);
+        else if (mPage == 2) {
 
+            view = inflater.inflate(R.layout.outside, container, false);
+            flower1 = (ImageButton)view.findViewById(R.id.imageButton1);
+            flower1.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    dialog();
+                }
+            });
+            //flower1 = (ImageButton)findViewById(R.id.imageButton1);
         }
-
         return view;
        /* View view = inflater.inflate(R.layout.fragment_page, container, false);
         TextView textView = (TextView) view;
         textView.setText("Fragment #" + mPage);
         return view;*/
+
+    }
+
+    protected void dialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        builder.setMessage("你离散会挂吗？");
+
+        builder.setTitle("Warning");
+
+        builder.setPositiveButton("会", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+
+
+            }
+        });
+
+        builder.setNegativeButton("会", new DialogInterface.OnClickListener() {
+
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss();
+            }
+        });
+
+        builder.create().show();
     }
 
 }
